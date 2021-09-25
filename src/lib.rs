eca4ce62a6aa21b57112a568c5c72919e9150937
gameltb <gamegccltb@gmail.com>
2021-09-25 23:47:58 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ enum Strsize {
 impl LuaPickle {
     fn read(&mut self) -> Result<(u8, Value), Box<dyn std::error::Error>> {
         let mut type_char = [0u8; 1];
-        self.file.read_exact(&mut type_char)?;
+        self.file.read_exact(&mut type_char).unwrap();
 
         let mut type_char = type_char[0];
         let content = match type_char {
@@ -24,63 +24,68 @@ impl LuaPickle {
             0x02 => Value::Bool(false),
             0x03 => {
                 let mut buff = [0u8; 1];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 Value::Number(Number::from(buff[0]))
             }
             0x04 => {
                 let mut buff = [0u8; 2];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let v = u16::from_le_bytes(buff);
                 Value::Number(Number::from(v))
             }
             0x05 => {
                 let mut buff = [0u8; 4];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let v = u32::from_le_bytes(buff);
                 Value::Number(Number::from(v))
             }
             0x06 => {
                 let mut buff = [0u8; 8];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let v = u64::from_le_bytes(buff);
                 Value::Number(Number::from(v))
             }
             0x07 => {
                 let mut buff = [0u8; 8];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let v = f64::from_le_bytes(buff);
-                Value::Number(Number::from_f64(v).ok_or("not valid 64bit float")?)
+                Value::Number(Number::from_f64(v).ok_or("not valid 64bit float").unwrap())
             }
-            0x08 => self.readstr(Strsize::Byte)?,
-            0x09 => self.readstr(Strsize::Halfword)?,
+            0x08 => self.readstr(Strsize::Byte).unwrap(),
+            0x09 => self.readstr(Strsize::Halfword).unwrap(),
             0x0b => {
                 let mut buff = [0u8; 4];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let count = u32::from_le_bytes(buff);
-                self.readtab(count)?
+                self.readtab(count).unwrap()
             }
             0x0d => {
-                let (_, key) = self.read()?;
-                let (_, content) = self.read()?;
+                let (_, key) = self.read().unwrap();
+                let (_, content) = self.read().unwrap();
                 let mut dict: Map<String, Value> = Map::new();
                 dict.insert(key.to_string(), content);
                 Value::Object(dict)
             }
+            0x0e => {
+                let mut buff = [0u8; 1];
+                self.file.read_exact(&mut buff).unwrap();
+                Value::Number(Number::from(buff[0]))
+            }
             0x0f => {
                 let mut buff = [0u8; 2];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let v = u16::from_le_bytes(buff);
                 Value::Number(Number::from(v))
             }
             0x10 => {
                 let mut buff = [0u8; 4];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 let v = u32::from_le_bytes(buff);
                 Value::Number(Number::from(v))
             }
             _ => {
-                let pos = self.file.seek(SeekFrom::Current(0))?;
-                println!("unhandle type:{}\npos:{}", type_char, pos);
+                let pos = self.file.seek(SeekFrom::Current(0)).unwrap();
+                panic!("unhandle type:{} pos:{}", type_char, pos);
                 type_char = 0;
                 Value::default()
             }
@@ -92,19 +97,19 @@ impl LuaPickle {
         let mut dict: Map<String, Value> = Map::new();
         if count > 0 {
             for index in 1..count + 1 {
-                let (_, coutent) = self.read()?;
+                let (_, coutent) = self.read().unwrap();
                 dict.insert(index.to_string(), coutent);
             }
         }
         loop {
-            let (type_char, key) = self.read()?;
+            let (type_char, key) = self.read().unwrap();
             if type_char == 0 {
                 break;
             }
-            let (_, coutent) = self.read()?;
+            let (_, coutent) = self.read().unwrap();
             let k = match key {
-                Value::Number(_) => key.as_u64().ok_or("cast fail")?.to_string(),
-                Value::String(_) => key.as_str().ok_or("cast fail")?.to_string(),
+                Value::Number(_) => key.as_u64().ok_or("cast fail").unwrap().to_string(),
+                Value::String(_) => key.as_str().ok_or("cast fail").unwrap().to_string(),
                 _ => key.to_string(),
             };
             dict.insert(k, coutent);
@@ -115,12 +120,12 @@ impl LuaPickle {
         let strlen: u32 = match fmt {
             Strsize::Byte => {
                 let mut buff = [0u8; 1];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 buff[0] as u32
             }
             Strsize::Halfword => {
                 let mut buff = [0u8; 2];
-                self.file.read_exact(&mut buff)?;
+                self.file.read_exact(&mut buff).unwrap();
                 u16::from_le_bytes(buff) as u32
             }
         };
@@ -129,20 +134,20 @@ impl LuaPickle {
         self.file
             .by_ref()
             .take(strlen as u64)
-            .read_to_string(&mut string)?;
+            .read_to_string(&mut string).unwrap();
         Ok(Value::String(string))
     }
 }
 
 pub fn unpickle(filepath: &str, skipbyte: u64) -> Result<Value, Box<dyn std::error::Error>> {
-    let file = OpenOptions::new().read(true).open(filepath)?;
+    let file = OpenOptions::new().read(true).open(filepath).unwrap();
 
     let mut file = BufReader::new(file);
 
-    file.seek(SeekFrom::Start(skipbyte))?;
+    file.seek(SeekFrom::Start(skipbyte)).unwrap();
 
     let mut an_pickle = LuaPickle { file };
-    let (_, dict) = an_pickle.read()?;
+    let (_, dict) = an_pickle.read().unwrap();
 
     Ok(dict)
 }
